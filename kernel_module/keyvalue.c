7cd0871be4e42641cef2193b46b8e06da039cb92
viswaraavi <viswaraavi@yahoo.in>
2016-10-29 13:20:06 -0400

    Avl tree added

--- kernel_module/keyvalue.c
+++ kernel_module/keyvalue.c
@@ -42,7 +42,261 @@
 #include <linux/moduleparam.h>
 #include <linux/poll.h>
 
+
+#define NULL (void*)0 
+
 unsigned transaction_id;
+
+struct node
+{
+    int key;
+    char *data;
+    struct node *left;
+    struct node *right;
+    int height;
+};
+ 
+
+ 
+
+int height(struct node *N)
+{
+    if (N == NULL)
+        return 0;
+    return N->height;
+}
+ 
+
+int max(int a, int b)
+{
+    return (a > b)? a : b;
+}
+ 
+
+struct node* newNode(int key,char* data)
+{
+    struct node* node = (struct node*)
+                        kmalloc(sizeof(struct node));
+    node->key   = key;
+    node->left   = NULL;
+    node->right  = NULL;
+    node->data =   data;
+    node->height = 1; 
+    return(node);
+}
+ 
+
+struct node *rightRotate(struct node *y)
+{
+    struct node *x = y->left;
+    struct node *T2 = x->right;
+ 
+ 
+    x->right = y;
+    y->left = T2;
+ 
+ 
+    y->height = max(height(y->left), height(y->right))+1;
+    x->height = max(height(x->left), height(x->right))+1;
+ 
+
+    return x;
+}
+ 
+
+struct node *leftRotate(struct node *x)
+{
+    struct node *y = x->right;
+    struct node *T2 = y->left;
+ 
+ 
+    y->left = x;
+    x->right = T2;
+ 
+   
+    x->height = max(height(x->left), height(x->right))+1;
+    y->height = max(height(y->left), height(y->right))+1;
+ 
+   
+    return y;
+}
+ 
+
+int getBalance(struct node *N)
+{
+    if (N == NULL)
+        return 0;
+    return height(N->left) - height(N->right);
+}
+ 
+struct node* insert(struct node* node, int key,char *value)
+{
+   
+    if (node == NULL)
+        return(newNode(key,value));
+
+ 
+    if (key < node->key)
+        node->left  = insert(node->left, key,value);
+    else
+        node->right = insert(node->right, key,value);
+ 
+
+    node->height = max(height(node->left), height(node->right)) + 1;
+ 
+   
+    int balance = getBalance(node);
+ 
+   
+ 
+
+    if (balance > 1 && key < node->left->key)
+        return rightRotate(node);
+ 
+   
+    if (balance < -1 && key > node->right->key)
+        return leftRotate(node);
+ 
+
+    if (balance > 1 && key > node->left->key)
+    {
+        node->left =  leftRotate(node->left);
+        return rightRotate(node);
+    }
+ 
+   
+    if (balance < -1 && key < node->right->key)
+    {
+        node->right = rightRotate(node->right);
+        return leftRotate(node);
+    }
+ 
+   
+    return node;
+}
+ 
+
+struct node * minValueNode(struct node* node)
+{
+    struct node* current = node;
+ 
+    
+    while (current->left != NULL)
+        current = current->left;
+ 
+    return current;
+}
+ 
+struct node* deleteNode(struct node* root, int key)
+{
+   
+ 
+    if (root == NULL)
+        return root;
+ 
+
+    if ( key < root->key )
+        root->left = deleteNode(root->left, key);
+
+ 
+
+    else if( key > root->key )
+        root->right = deleteNode(root->right, key);
+ 
+ 
+    else
+    {
+       
+        if( (root->left == NULL) || (root->right == NULL) )
+        {
+            struct node *temp = root->left ? root->left : root->right;
+ 
+   
+            if(temp == NULL)
+            {
+                temp = root;
+                root = NULL;
+            }
+            else 
+             *root = *temp; 
+ 
+            free(temp);
+        }
+        else
+        {
+           
+            struct node* temp = minValueNode(root->right);
+ 
+           
+            root->key = temp->key;
+ 
+           
+            root->right = deleteNode(root->right, temp->key);
+        }
+    }
+ 
+   
+    if (root == NULL)
+      return root;
+ 
+    
+    root->height = max(height(root->left), height(root->right)) + 1;
+ 
+    
+    int balance = getBalance(root);
+ 
+    
+    if (balance > 1 && getBalance(root->left) >= 0)
+        return rightRotate(root);
+ 
+
+    if (balance > 1 && getBalance(root->left) < 0)
+    {
+        root->left =  leftRotate(root->left);
+        return rightRotate(root);
+    }
+ 
+
+    if (balance < -1 && getBalance(root->right) <= 0)
+        return leftRotate(root);
+ 
+
+    if (balance < -1 && getBalance(root->right) > 0)
+    {
+        root->right = rightRotate(root->right);
+        return leftRotate(root);
+    }
+ 
+    return root;
+}
+
+ 
+
+
+ 
+struct node* search(struct node* root,int key)
+{
+if(root==NULL)
+{
+return NULL;
+}
+if(root->key==key)
+{
+return root;
+}
+if(root -> key < key )
+{
+return search(root->right,key );
+}
+if(root -> key > key )
+{
+return search(root->left,key );
+}
+
+}
+
+
+
 static void free_callback(void *data)
 {
 }
@@ -129,3 +383,4 @@ MODULE_LICENSE("GPL");
 MODULE_VERSION("0.1");
 module_init(keyvalue_init);
 module_exit(keyvalue_exit);
+
