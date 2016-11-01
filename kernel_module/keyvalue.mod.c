663ee2126a07ae4a9628c2f577e13c66ddfee960
root <root@bn20-99.dcs.mcnc.org>
2016-11-01 18:50:40 -0400

    sem

--- kernel_module/keyvalue.mod.c
+++ kernel_module/keyvalue.mod.c
@@ -22,9 +22,7 @@ __attribute__((section("__versions"))) = {
 	{ 0xc7b102a, __VMLINUX_SYMBOL_STR(misc_register) },
 	{ 0x27e1a049, __VMLINUX_SYMBOL_STR(printk) },
 	{ 0x4f8b5ddb, __VMLINUX_SYMBOL_STR(_copy_to_user) },
-	{ 0x71e3cecb, __VMLINUX_SYMBOL_STR(up) },
 	{ 0x37a0cba, __VMLINUX_SYMBOL_STR(kfree) },
-	{ 0xf22449ae, __VMLINUX_SYMBOL_STR(down_interruptible) },
 	{ 0x4f6b400b, __VMLINUX_SYMBOL_STR(_copy_from_user) },
 	{ 0xd2b09ce5, __VMLINUX_SYMBOL_STR(__kmalloc) },
 	{ 0xdf3deb56, __VMLINUX_SYMBOL_STR(kmem_cache_alloc_trace) },
@@ -38,4 +36,4 @@ __attribute__((section(".modinfo"))) =
 "depends=";
 
 
-MODULE_INFO(srcversion, "C2C61AF9170DAA45DEEB381");
+MODULE_INFO(srcversion, "460C7AAF0D5D8148E257D48");
